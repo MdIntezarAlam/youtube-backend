721376ab2a9ea843bf8f11f84e14a4392aa182de
Intezar Alam <tanjavoorathefe@gmail.com>
2024-09-04 11:34:26 +0530

    fixesss

--- scripts/build-functions.js
+++ scripts/build-functions.js
@@ -1,20 +1,8 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 
-// Define source and destination directories
-const srcDir = path.join(__dirname, "../src/functions");
-const destDir = path.join(__dirname, "../dist/functions");
+const functionsDir = path.join(__dirname, "../dist/functions");
+const sourceDir = path.join(__dirname, "../src/functions");
 
-// Ensure destination directory exists
-if (!fs.existsSync(destDir)) {
-  fs.mkdirSync(destDir, { recursive: true });
-}
-
-// Copy files from src to dest
-fs.readdirSync(srcDir).forEach((file) => {
-  const srcPath = path.join(srcDir, file);
-  const destPath = path.join(destDir, file);
-  fs.copyFileSync(srcPath, destPath);
-});
-
+fs.copySync(sourceDir, functionsDir);
 console.log("Functions have been copied to the dist/functions directory.");
