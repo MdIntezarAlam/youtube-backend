8e6f905df7af0eef08c68599d0dc48893518704d
Intezar Alam <tanjavoorathefe@gmail.com>
2024-09-04 10:32:35 +0530

    fixes

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,26 +1,4 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const userRoutes = require("./path/to/your/userRoutes");
+const serverless = require("serverless-http");
+const app = require("../app").default;
 
-// Initialize Express
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
-// Use the routes
-app.use("/api", userRoutes);
-
-// Export the handler
-module.exports.handler = require("serverless-http")(app);
+module.exports.handler = serverless(app);
